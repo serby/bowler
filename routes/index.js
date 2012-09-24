4aecfd311f186592f70a658a9f0359e5dd5cfcdf
Paul Serby <paul.serby@clock.co.uk>
2012-09-24 01:19:36 +0100

    Fixed info refresh

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,13 @@
 var http = require('http')
   , os = require('os')
-  , info = {
+  ;
+module.exports = function (req, res) {
+  var  info = {
     arch: process.arch,
       platform: process.platform,
       memoryUsage: process.memoryUsage(),
       uptime: process.uptime(),
       loadAvg: os.loadavg()
     };
-
-module.exports = function (req, res) {
   res.render('index', { title: 'Bowler', info: JSON.stringify(info) });
 };
