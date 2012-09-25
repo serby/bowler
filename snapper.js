3e4e9edc613db598712fa2180e3f7e838ec63d07
root <root@raspberrypi.(none)>
2012-09-25 23:20:45 +0000

    Only snap when ready

--- snapper.js
+++ snapper.js
@@ -1,10 +1,17 @@
 var exec = require('child_process').exec
-  , child;
+  , child
+	, ready = false
+	;
 
-child = exec('streamer -c /dev/video0 -s 544x288 -b 16 -o public/images/cam.jpeg', function (error, stdout, stderr) {
-  console.log('stdout: ' + stdout);
-  console.log('stderr: ' + stderr);
-  if (error !== null) {
-    console.log('exec error: ' + error);
-  }
-});
+function snap() {
+	if (!ready) {
+		return false;
+	}
+	ready = false;
+	child = exec('streamer -c /dev/video0 -s 544x288 -b 16 -o ' + __dirname + '/public/images/cam.jpeg', function (error, stdout, stderr) {
+		ready = true;
+		console.log('Snap', error, stdout, stderr);  
+	});
+}
+
+setInterval(snap, 2000);
