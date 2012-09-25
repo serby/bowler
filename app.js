d36f536b94d1df7cddb976a533a8c96f4444ad01
Paul Serby <paul.serby@clock.co.uk>
2012-09-25 21:51:40 +0100

    Adding snapper

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
-
 /**
  * Module dependencies.
  */
 
 var express = require('express')
   , http = require('http')
-  , path = require('path');
+  , path = require('path')
+  , app = express()
+  , createLightSwitch = require('./light-switch/light-switch')
+  , lightSwitch
+  , jarre = require('./jarre')()
+  ;
 
-var app = express();
+if (app.get('env') === 'development') {
+  lightSwitch = createLightSwitch(require('./light-switch/mock-simple-gpio')());
+} else {
+  lightSwitch = createLightSwitch();
+}
 
-app.configure(function(){
+jarre.add(require('./light-show')(lightSwitch));
+
+app.configure(function() {
+
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
@@ -19,6 +30,7 @@ app.configure(function(){
   app.use(express.methodOverride());
   app.use(app.router);
   app.use(express.static(path.join(__dirname, 'public')));
+
 });
 
 app.configure('development', function(){
@@ -26,7 +38,20 @@ app.configure('development', function(){
 });
 
 app.get('/', require('./routes/index'));
-app.get('/light-show', require('./routes/light-show'));
+
+app.get('/light-show', function (req, res) {
+
+  try {
+
+    jarre.pickup('lightShow').start(jarre.drop);
+    res.redirect('/');
+
+  } catch (e) {
+    res.send('Light show already running');
+    res.end();
+  }
+
+});
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port http://localhost:' + app.get('port'));
---
+++ snapper.js
@@ -0,0 +1,10 @@
+var exec = require('child_process').exec
+  , child;
+
+child = exec('streamer -c /dev/video0 -s 544x288 -b 16 -o public/images/cam.jpeg', function (error, stdout, stderr) {
+  console.log('stdout: ' + stdout);
+  console.log('stderr: ' + stderr);
+  if (error !== null) {
+    console.log('exec error: ' + error);
+  }
+});
