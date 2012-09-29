06481154e098f7eeda4aa441bb0554c7f8eee73b
Paul Serby <paul.serby@clock.co.uk>
2012-09-29 04:42:09 +0100

    Toggle comments on env

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.configure(function() {
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   app.use(express.favicon());
-  app.use(express.logger('dev'));
+  app.use(express.logger((app.get('env') === 'development') ? 'dev' : undefined));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
